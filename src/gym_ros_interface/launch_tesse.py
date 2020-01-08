34c138516d1cfcc41b88b1c388556e4eb75ab367
Zachary Ravichandran <zachary.ravichandran@ll.mit.edu>
2020-01-07 20:58:12 -0500

    initial node and launch file to run tesse and kimera to interface with a gym environment

---
+++ src/gym_ros_interface/launch_tesse.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+
+import rospy
+import subprocess
+import time
+
+
+class LaunchTesse:
+    """ Node to launch TESSE via a subprocess """
+    def __init__(self):
+        tesse_path = rospy.get_param("~tesse_path")
+        listen_port = rospy.get_param("~position_port", "9000")
+        send_port = rospy.get_param("~send_port", "9000")
+        width = rospy.get_param("~width", 320)
+        height = rospy.get_param("~height", 240)
+
+        self.proc = subprocess.Popen(
+                [
+                    tesse_path,
+                    "--listen_port",
+                    listen_port,
+                    "--send_port",
+                    send_port,
+                    "--set_resolution",
+                    str(width),
+                    str(height)
+                ]
+            )
+
+        rospy.on_shutdown(self.on_exit)
+        rospy.spin()
+
+    def on_exit(self):
+        """ Kill subprocess upon exit"""
+        self.proc.kill()
+
+
+if __name__ == "__main__":
+    rospy.init_node("LaunchTesse_node")
+    node = LaunchTesse()
+    node.spin()
